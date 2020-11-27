e579c735fa4fe5ddbf8c43d8dcc799af6e4685fb
Shamim Hussain <snirjhar@gmail.com>
2020-11-27 02:52:36 -0500

    removed tfp

--- vae_layers.py
+++ vae_layers.py
@@ -3,32 +3,27 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import tensorflow_probability as tfp
 
 
-# def kl_gaussians(mu_1,sigma_1, mu_2, sigma_2):
-#     return tf.math.log(sigma_2/sigma_1) \
-#             + (sigma_1**2 + (mu_1-mu_2)**2)/(2*sigma_2**2) - 0.5
+def kl_gaussians(mu_1,sigma_1, mu_2, sigma_2):
+    return tf.math.log(sigma_2/sigma_1) \
+            + (sigma_1**2 + (mu_1-mu_2)**2)/(2*sigma_2**2) - 0.5
 
+def kl_std_gaussian(mu, sigma):
+    return -tf.math.log(sigma) + 0.5*(sigma**2 + mu**2) - 0.5
 
 
 class SamplingLoss(layers.Layer):
-    def build(self,input_shapes):
-        _,mean_shape,std_shape=input_shapes
-        self.prior=tfp.distributions.Normal(tf.zeros(mean_shape[-1]),
-                                            tf.ones(std_shape[-1]))
-        self.built=True
         
     def call(self,inputs):
-        x,mean,std=inputs
-        posterior=tfp.distributions.Normal(mean, tf.abs(std)+1e-9)
+        x,mean_q,std_q=inputs
         
-        compress_loss = tf.reduce_sum(tf.reduce_mean(
-            tfp.distributions.kl_divergence(posterior,self.prior),axis=0))
+        kl_div = kl_std_gaussian(mean_q, std_q)        
+        compress_loss = tf.reduce_sum(tf.reduce_mean(kl_div,axis=0))
         
         self.add_loss(compress_loss)
         
-        return x,mean,std
+        return inputs
 
 
 
@@ -36,11 +31,9 @@ class SamplingLoss(layers.Layer):
 class ConditionalSamplingLoss(layers.Layer):        
     def call(self,inputs):
         _, mean_q,std_q, mean_p,std_p = inputs
-        prior = tfp.distributions.Normal(mean_p, tf.abs(std_p)+1e-9)
-        posterior = tfp.distributions.Normal(mean_q, tf.abs(std_q)+1e-9)
         
-        compress_loss = tf.reduce_sum(tf.reduce_mean(
-            tfp.distributions.kl_divergence(posterior,prior),axis=0))
+        kl_div = kl_gaussians(mean_q, std_q, mean_p, std_p)
+        compress_loss = tf.reduce_sum(tf.reduce_mean(kl_div,axis=0))
         
         self.add_loss(compress_loss)
         
